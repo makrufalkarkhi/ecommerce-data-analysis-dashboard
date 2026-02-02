3895e12f27c9a8858d2fb3075a86a758fb8e19d0
Makruf <makruf.119140075@student.itera.ac.id>
2026-02-03 00:15:40 +0700

    fix csv path for streamlit cloud

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -15,7 +15,7 @@ st.set_page_config(
 # Load data
 @st.cache_data
 def load_data():
-    return pd.read_csv("main_data.csv")
+    return pd.read_csv("dashboard/main_data.csv")
 
 all_df = load_data()
 
